de05d625a2713c8ed4b560712bbeb8d22f4b0647
carr <tomislav@infinum.hr>
2010-01-02 05:14:51 +0100

    Updated readme, upgraded to unobtrusive JS, removed bunch of unnecessary code.

--- lib/tri_state.rb
+++ lib/tri_state.rb
@@ -1,41 +1,10 @@
-# usage: include in custom form builder
-#   f.tri_state :some_field
-# 
-# field needs to be :integer
-# TODO
-# * izbaciti collection i staviti hidden field 
-# * probati na jedno mjesto staviti putanje do slika
 module TriState
-  # TODO nisam siguran da li bas ovako, nece raditi normalna verzija, bez f.tri_state
-  def tri_state(method, options = {}, checked_value = "1", unchecked_value = "0")
-    require 'ostruct'
-    collection = [
-      OpenStruct.new(:value => 1, :text => 'true'),
-      OpenStruct.new(:value => 0, :text => 'false')      
-    ]
-    
+  def tri_state(method, options = {})    
+    id = "#{self.object_name}_#{method}"
     options.merge!({:include_blank => true})
     
-    arr = ""
-    arr << collection_select(method, collection, :value, :text, options, 
-                              {:style => 'display: none;', :onclick => tri_state_onclick(method)})
-    arr << tri_state_link(method)
-  end
-  
-  def tri_state_link(method)
-    value = self.object.send(method)
+    arr = select(method, {:true => 1, :false => 0}, options, {:id => id})
     
-    state_image = 'question_mark' if value.nil?
-    state_image = 'cancel' if value==0
-    state_image = 'accept' if value==1
-    
-    @template.image_tag("/images/icons/#{state_image}.png", 
-                        :onclick => tri_state_onclick(method),
-                        :id => "#{self.object_name}_#{method}_tri_state", 
-                        :class => 'tri_state_link')
-  end
-  
-  def tri_state_onclick(method)
-    "TriState.toggle('#{self.object_name}_#{method}')"
+    arr << @template.javascript_tag("TriState.init('#{id}')")
   end
 end
--- public/javascripts/tri_state.js
+++ public/javascripts/tri_state.js
@@ -1,24 +1,34 @@
 var TriState = {
-  toggle : function(id){
-    cycle = ["1", "0", ""];
-    value = $F(id);
+  images: {
+    '1': '/images/icons/accept.png', 
+    '0': '/images/icons/cancel.png', 
+    '': '/images/icons/question_mark.png'
+  },
+  
+  transitions: {
+    "1": "0",
+    "0": "",
+    "": "1"
+  },
+  
+  init : function(id){
+    var element = $(id);
+    var link_id = id + "_link";
     
-    if(value.blank()){
-      newState = "1";
-    }else{
-      if(value == '1'){
-        newState = "0";
-      }else{
-        newState = '';
-      }
+    Element.replace(id, '<input type="hidden" id="' + id + '" name="' + element.name + '" value="' + element.value + '"/>');
+    Element.insert(id, {after : 
+                    '<img id="' + link_id + '" src="' + this.images[element.value] + '" class="tri_state" />'
+                   });
+    
+    $(id).onclick = function(){
+      TriState.toggle(id, link_id);
     }
-    
-    // TODO Izbaciti collection i staviti hidden field
-    Select.setByValue(id, newState);
-    
-    images = {'1': 'accept', '0': 'cancel', '': 'question_mark'}
-    
-    link = $(id + "_tri_state")
-    link.src = '/images/icons/' + images[newState] + ".png"
+    $(link_id).onclick = $(id).onclick;
+  },
+  
+  toggle : function(id, link_id){
+    var newState = this.transitions[$(id).value];
+    $(id).value = newState;    
+    $(link_id).src = this.images[newState];
   }
 }
